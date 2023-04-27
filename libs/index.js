c49a95a56211909240d061dc3c96c0eb72a5f510
Mervin <mengqing723@gmail.com>
2023-04-27 20:21:49 +0800

    feat: add libs

---
+++ libs/index.js
@@ -0,0 +1,71 @@
+const through = require('through2')
+const PluginError = require('plugin-error')
+const argv = require('minimist')(process.argv.slice(2))
+const loadEnv = require('./loadEnv')
+
+const PLUGIN_NAME = 'gulp-env-loader'
+
+function replaceContent(content, env, isVar) {
+  Object.entries(env).forEach(([key, value]) => {
+    // content = content.replace(new RegExp(`process\\.env\\.${key}`, 'g'), isVar ? `'${value}'` : value)
+
+    // 完整匹配 env, ${ env }, 'env', "env", {{ env }}, { env }
+    // 使用捕获组来匹配这几种格式
+    // env 表示匹配 env 字符串，
+    // \$\{\s*env\s*\} 表示匹配 ${ env } 格式的字符串，
+    // '(?:[^'\\]*(?:\\.[^'\\]*)*)' 表示匹配 ' 包含的字符串，
+    // "([^"\\]*(?:\\.[^"\\]*)*)" 表示匹配 " 包含的字符串，
+    // \{\{\s*env\s*\}\} 表示匹配 {{ env }} 格式的字符串，
+    // \{(?:\s*)env(?:\s*)\} 表示匹配 { env } 格式的字符串。
+    content = content.replace(new RegExp(`(process\\.env\\.${key}|\\$\\{\\s*process\\.env\\.${key}\\s*\\}|'(?:[^'\\\\]*(?:\\\\.[^'\\\\]*)*)'|\"([^\"\\\\]*(?:\\\\.[^\"\\\\]*)*)\"|\\{\\{\\s*process\\.env\\.${key}\\s*\\}\\}|\\{(?:\\s*)process\\.env\\.${key}(?:\\s*)})`, 'g'), (match) => {
+      const _env = `process.env.${key}`
+      if (match === _env) {
+        return isVar ? `'${value}'` : value
+      } else if (match.startsWith('{') || match.startsWith('${')) {
+        return value
+      } else {
+        return match.replace(`process.env.${key}`, value)
+      }
+    })
+  })
+  return content
+}
+
+module.exports = function (config = {}) {
+  const conf = {
+    mode: argv.mode
+  }
+  if (config && typeof config === 'object') {
+    conf.mode = config.mode ?? argv[config.modeKey || 'mode']
+    conf.path = config.path
+  } else if (typeof config === 'string') {
+    conf.path = config
+  }
+
+  const env = loadEnv(conf)
+
+  function envInject(options = {}) {
+    return through.obj((file, enc, callback) => {
+      if (file.isNull()) {
+        return callback(null, file)
+      }
+
+      if (file.isStream()) {
+        return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+      }
+
+      try {
+        const contents = file.contents.toString(enc)
+        const replacedContents = replaceContent(contents, env, options.isVar ?? true)
+        file.contents = Buffer.from(replacedContents)
+      } catch (err) {
+        return callback(new PluginError(PLUGIN_NAME, err))
+      }
+
+      callback(null, file)
+    })
+  }
+
+  envInject.env = env
+  return envInject
+}
---
+++ libs/loadEnv.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const fs = require('fs')
+const { parse: envParse } = require('dotenv')
+const { expand } = require('dotenv-expand')
+
+function tryStatSync(file) {
+  try {
+    return fs.statSync(file, { throwIfNoEntry: false })
+  } catch (e) {
+    // return false
+  }
+}
+
+function getEnvFiles(mode, envDir) {
+  envDir = envDir || process.cwd()
+  const files = [
+    `.env`,
+    `.env.local`,
+  ]
+  if (mode) {
+    files.push(`.env.${mode}`)
+    files.push(`.env.${mode}.local`)
+  }
+
+  return files.map(file => path.resolve(envDir, file))
+}
+
+/**
+ * 加载环境变量
+ * @param {object | string} config 配置
+ * @param {string} config.path 文件路径
+ * @param {string} config.mode 模式
+ */
+function loadEnv(config = {}) {
+  let conf = {}
+  let envFiles = []
+
+  if (config && typeof config === 'object') {
+    conf = { ...config }
+  } else if (typeof config === 'string') {
+    conf.path = config
+  }
+
+  if (conf.mode === 'local') {
+    throw new Error(`"local" cannot be used as a mode name because it conflicts with the .local postfix for .env files.`)
+  }
+
+  if (conf.path) {
+    if (tryStatSync(conf.path)?.isFile()) {
+      envFiles.push(conf.path)
+    } else if (tryStatSync(conf.path)?.isDirectory()) {
+      envFiles = getEnvFiles(conf.mode, conf.path)
+    }
+  } else {
+    envFiles = getEnvFiles(conf.mode)
+  }
+
+  const parsed = Object.fromEntries(
+    envFiles.flatMap((file) => {
+      if (!tryStatSync(file)?.isFile()) return []
+
+      return Object.entries(envParse(fs.readFileSync(file)))
+    })
+  )
+
+  const result = expand({ parsed })
+  if (result.error) {
+    throw result.error
+  }
+
+  return result.parsed
+}
+
+module.exports = loadEnv
